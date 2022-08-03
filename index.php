e061cbec42b33afbad8b5104018f139707ebdca8
Cy3r3n <aurnob12@gmail.com>
2022-08-04 00:03:29 +0600

    First Commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title> Boi-BechaKena </title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="https://ajax.com.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Boi-BechaKena</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto mb-2 mb-lg-2">
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+      </li>
+
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Categories
+        </a>
+        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <li><a class="dropdown-item" href="#">Thriller</a></li>
+          <li><hr class="dropdown-divider"></li>
+          <li><a class="dropdown-item" href="#">Comedy</a></li>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Comedy</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Fantasy</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Romance</a>
+        </ul>
+      </li>
+     
+    </ul>
+  </div>
+</nav>
+
+</body>
+</html>
